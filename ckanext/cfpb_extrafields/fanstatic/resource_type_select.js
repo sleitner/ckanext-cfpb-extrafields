7393d0c288c524089e0f43b35add925c175abefe
Chris Contolini <christopher.contolini@cfpb.gov>
2015-08-04 14:36:30 -0400

    Move resource type selection into sidebar

--- ckanext/cfpb_extrafields/fanstatic/resource_type_select.js
+++ ckanext/cfpb_extrafields/fanstatic/resource_type_select.js
@@ -1,8 +1,11 @@
 // this generates a warning about unsaved data from CKAN
 // public/base/javascript/modules/basic-form.js
 $(function() {
-    $('#field-resource_type').change( function(event) {
-        event.stopPropagation();
-        $( "#resource_save" ).trigger( "click" );
+    $('#resource-switcher a').on('click', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      var type = $(this).attr('href').replace('#', '');
+      $('#field-resource_type').val(type);
+      $('#resource_save').trigger('click');
     });
 });
